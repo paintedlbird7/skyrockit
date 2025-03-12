bfd485a72c91a4074db5bfdb72199451ec80dc69
Rosa M Perez <rosaperezlarios@gmail.com>
2025-03-11 20:39:03 -0700

    wip: working on create posts

--- controllers/applications.js
+++ controllers/applications.js
@@ -8,17 +8,56 @@ const User = require('../models/user.js');
 //     res.send('Hello applications index route!');
 //   });
 
+// controllers/applications.js
+
 router.get('/', async (req, res) => {
-    try {
-      res.render('applications/index.ejs');
-    } catch (error) {
-      console.log(error);
-      res.redirect('/');
-    }
-  });
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Render index.ejs, passing in all of the current user's
+    // applications as data in the context object.
+    res.render('applications/index.ejs', {
+      applications: currentUser.applications,
+    });
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// router.get('/', async (req, res) => {
+//     try {
+//       res.render('applications/index.ejs');
+//     } catch (error) {
+//       console.log(error);
+//       res.redirect('/');
+//     }
+//   });
   
 router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
+});
+
+// controllers/applications.js`
+
+//TODO: tested the create post, can't find it in mongo atlas 
+router.post('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
