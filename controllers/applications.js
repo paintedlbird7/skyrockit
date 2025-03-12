dddba4ef86820d1951abf469980e1791356e0ea0
Rosa M Perez <rosaperezlarios@gmail.com>
2025-03-11 18:49:12 -0700

    can sign in plus displays nav partials when signin

---
+++ controllers/applications.js
@@ -0,0 +1,20 @@
+// controllers/applications.js
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+// router.get('/', (req, res) => {
+//     res.send('Hello applications index route!');
+//   });
+
+router.get('/', async (req, res) => {
+    try {
+      res.render('applications/index.ejs');
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const app = express();
+const applicationsController = require('./controllers/applications.js');
+// Require our new middleware
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+// Require controllers
 const authController = require('./controllers/auth.js');
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT || 3000;
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// Middleware setup
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
@@ -28,22 +35,38 @@ app.use(
   })
 );
 
+// Use passUserToView middleware to make user available in views
+app.use(passUserToView);
+
+// GET Build the Applications Index Page
+app.get('/', (req, res) => {
+  // Check if the user is signed in
+  if (req.session.user) {
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
+  }
+});
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+// removed the VIP lounge & updated the index.ejs to skyrockit
 
+// Authentication routes
 app.use('/auth', authController);
+// Use isSignedIn middleware to protect routes that come after this
+app.use(isSignedIn);
+app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController);
 
+// Start the server
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+  console.log(`The express app is running on port ${port}!`);
 });
